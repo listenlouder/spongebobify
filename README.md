53567d66baa6e3c730150a6e159706afcc47332b
listenlouder <wbradford@republicwireless.com>
2018-10-25 15:53:07 -0400

    making pycharm happy(ish)

---
+++ README.md
@@ -0,0 +1,3 @@
+Dumb tool to make fun of my idiot friends.
+
+Need to try and make this into a slack/discord addon
--- spongebobify.py
+++ spongebobify.py
@@ -1,6 +1,5 @@
 import sys
 from PIL import Image, ImageDraw, ImageFont
-import os
 import textwrap
 
 
@@ -10,7 +9,7 @@ def spongebobify(sentence):
 	
 	for word in words:
 		for pos, letter in enumerate(word):
-			if pos%2 == 0:
+			if pos % 2 == 0:
 				final_sentence += letter.lower()
 			else:
 				final_sentence += letter.upper()
@@ -33,11 +32,11 @@ def build_image(spongbobified_sentence):
 			(w, h) = draw.textsize(line, font=font)
 			x = (W-w)/2
 
-			draw.text((x-2, y-2), line,(0,0,0),font=font)
-			draw.text((x+2, y-2), line,(0,0,0),font=font)
-			draw.text((x+2, y+2), line,(0,0,0),font=font)
-			draw.text((x-2, y+2), line,(0,0,0),font=font)
-			draw.text((x, y), line, (255,255,255), font=font)
+			draw.text((x-2, y-2), line, (0, 0, 0), font=font)
+			draw.text((x+2, y-2), line, (0, 0, 0), font=font)
+			draw.text((x+2, y+2), line, (0, 0, 0), font=font)
+			draw.text((x-2, y+2), line, (0, 0, 0), font=font)
+			draw.text((x, y), line, (255, 255, 255), font=font)
 
 			y += font.getsize(line)[1]
 	
